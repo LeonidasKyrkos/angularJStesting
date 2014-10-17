a66d9e873a2beb8d3ab4f7ffe6c7ec689291d0c5
Leo Kyrkos <leo@redsnapper.net>
2014-10-17 17:40:13 +0100

    initial commit of angular js testing environment

---
+++ app/js/src/angular.js
@@ -0,0 +1,12 @@
+// Angular stuff //
+
+(function(){
+
+	console.log('test');
+
+
+}());
+
+var MainController = function($scope) {
+	$scope.message = "Hello, Angular!";
+};
---
+++ app/js/src/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var $ = require('jquery');
+
+
+// Angular stuff //
+
+(function(){
+
+}());
+
+var MainController = function($scope) {
+	$scope.message = "Hello, Angular!";
+};
